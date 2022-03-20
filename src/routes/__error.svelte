8874258211984ea97ed87e9a83fc0109facc1aa4
f-elix <guerinfelix08@gmail.com>
2022-03-20 12:42:26 -0400

    initial commit

---
+++ src/routes/__error.svelte
@@ -0,0 +1,10 @@
+<script context="module" lang="ts">
+	import type { Load } from './__error';
+
+	export const load: Load = ({ error, status }) => {
+		return {
+			error,
+			status
+		};
+	};
+</script>
