2f9588e858413d1881d76b2ab5de178242a95936
f-elix <guerinfelix08@gmail.com>
2022-04-07 10:27:04 -0400

    Fix build

--- src/lib/api/utils.ts
+++ src/lib/api/utils.ts
@@ -1,12 +1,3 @@
-import bcrypt from 'bcryptjs';
-
-const { genSalt, hash } = bcrypt;
-
-export const hashPassword = async (password: string) => {
-	const salt = await genSalt(10);
-	return hash(password, salt);
-};
-
 export const isInvalidPath = (path: string, invalidPaths: string[]) => {
 	return invalidPaths.find((p) => path.includes(p));
 };
